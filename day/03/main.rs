657f2c8fde965e25c6dcce3c0dabe4ad0b42cb9b
Sophie <>
2023-12-06 14:36:44 +0800

    Day 3 part 1?

--- day/03/main.rs
+++ day/03/main.rs
@@ -11,11 +11,35 @@ fn main()
 	let mut part_1_sum = 0;
 	let mut part_2_sum = 0;
 
-	// add an 'empty' pre & post line to avoid some hassle with the first and last lines later	
+	/* 
+	 * Add an 'empty' pre & post line to avoid some hassle with the first and last lines later
+	 */
+
 	let empty_line: String = ".".repeat(lines.first().unwrap().len());
 	
 	lines.insert(0, empty_line.clone());
 	lines.push(empty_line.clone());
+
+	/* 
+	 *		Add an 'empty' char to the beginning and end of every line to make life easier later
+	 */
+
+	let mut new_lines: Vec<String> = Vec::new();
+	
+	for line in lines
+	{
+	    let mut temp = line;
+	    
+	    temp.push('.');
+	    temp.insert(0,'.');
+	    
+	    new_lines.push(temp);
+	}	
+	let lines = new_lines;
+
+	/*
+	 *		Do the thing!
+	 */
 
 	let mut line_num = 0; 
 
@@ -49,13 +73,79 @@ fn main()
 fn part_1(prev_line: String, line: String, next_line: String) -> i32
 {
 	let mut sum: i32 = 0;
-		
-		// find a number, find the whole number
-		//
-		// check if there's a symbol to the left
-		// check if there's a symbol to the right
-		// check if there's a symbol above (and one to the left and right above)
-		// check if there's a symbol below (and one to the left and right below)
+	let mut num = 0;
+    
+    let mut is_part = false; // because we only add numbers to the sum if they're a part number
+
+    // get the strings' chars as a vector
+    let prev_chars: Vec<char> = prev_line.chars().collect();
+    let chars: Vec<char> = line.chars().collect();
+    let next_chars: Vec<char> = next_line.chars().collect();
+
+    for (i, c) in line.chars().enumerate()
+    {
+        // ignore the first and last characters (we added them earler)
+        if 0 < i && i < line.len() - 1
+        {
+            // is the current character a number?
+            if let Ok(digit) = c.to_string().parse::<i32>()
+            {
+                num = (num*10) + digit; // add the current character to the stored number
+                
+                // check if the number is ajacent to a symbol
+                // this is about to be very ugly and double check a bunch of chars
+                
+                let mut num_symbols = 0;
+                
+                num_symbols += is_symbol(*prev_chars.get(i-1).unwrap());
+                num_symbols += is_symbol(*prev_chars.get(i).unwrap());
+                num_symbols += is_symbol(*prev_chars.get(i+1).unwrap());
+                
+                num_symbols += is_symbol(*chars.get(i-1).unwrap());
+                num_symbols += is_symbol(*chars.get(i+1).unwrap());
+                
+                num_symbols += is_symbol(*next_chars.get(i-1).unwrap());
+                num_symbols += is_symbol(*next_chars.get(i).unwrap());
+                num_symbols += is_symbol(*next_chars.get(i+1).unwrap());
+                
+                if (num_symbols > 0)
+                {
+                    is_part = true;
+                }
+                
+                print!("{}", prev_chars.get(i).unwrap());
+                
+            }
+            else if num != 0
+            {
+                // if it IS a part, then add the current number to the sum
+                if is_part
+                {
+                    sum += num;
+                    is_part = false; // reset is_part
+                    num = 0; // reset num
+                }
+            }
+        }
+    }
 
 	return sum;
+}
+
+
+fn is_symbol(c: char) -> i32
+{
+    // is it a number?
+    if let Ok(_digit) = c.to_string().parse::<i32>()
+    {
+        return 0; // a number is not a symbol
+    }
+    
+    // is it 'empty'
+    if c == '.'
+    {
+        return 0;  // 'empty' is not a symbol
+    }
+    
+    return 1; // if we've gotten here then it's probably a symbol
 }
